49ceb91c63d0f7c840021a46afcdce50a1c3a534
Vrush2004 <vrushalichaube@gmail.com>
2024-12-21 11:30:16 +0530

    done

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function renderBoard(){
     
     allBoxes.forEach((boxElement, i) => {
         if(board[i] === 'O'){
-            boxElement.innerHTML = '<img src="./images/o.png" height="50px">';
+            boxElement.innerHTML = '<img src="./images/o.png" class="img">';
         }else if(board[i] === 'X'){
-            boxElement.innerHTML = '<img src="./images/x.png" height="50px">';
+            boxElement.innerHTML = '<img src="./images/x.png" class="img">';
         }else{
             boxElement.innerHTML = '';
         }
@@ -45,6 +45,50 @@ function resetBoard(){
     currentPlayerEle.innerText = currentPlayer;
 }
 
+function isPlayerPresent(places, player){
+    let result = true;
+    places.forEach((place) => {
+        if(board[place] != player){
+            result = false;
+        }
+    })
+    return result;
+}
+
 function checkForWinner(){
-    
+    if(isPlayerPresent([0,1,2], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([3,4,5], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([6,7,8], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([0,3,6], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([1,4,7], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([2,5,6], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([0,4,8], "O")){
+        alert("Player O wins");
+    }else if(isPlayerPresent([2,4,6], "O")){
+        alert("Player O wins");
+    }
+
+    if(isPlayerPresent([0,1,2], "X")){
+        alert("Player x wins");
+    }else if(isPlayerPresent([3,4,5], "X")){
+        alert("Player X wins");
+    }else if(isPlayerPresent([6,7,8], "X")){
+        alert("Player X wins");
+    }else if(isPlayerPresent([0,3,6], "X")){
+        alert("Player X wins");
+    }else if(isPlayerPresent([1,4,7], "X")){
+        alert("Player X wins");
+    }else if(isPlayerPresent([2,5,6], "X")){
+        alert("Player X wins");
+    }else if(isPlayerPresent([0,4,8], "X")){
+        alert("Player X wins");
+    }else if(isPlayerPresent([2,4,6], "X")){
+        alert("Player X wins");
+    }
 }
